001aa69de000687c0457edd31612d818c32ad177
Mumin8 <alhassanmumin8@gmail.com>
2022-11-07 02:48:21 -0800

    header file

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stdlib.h>
 
 /**
@@ -11,7 +12,6 @@
  */
 
 typedef struct listint_s
-
 {
 	const int n;
 	struct listint_s *prev;
@@ -21,4 +21,5 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 #endif
